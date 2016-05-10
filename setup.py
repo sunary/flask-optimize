3aa1db60f56bf7a2ad13ae192abb9880d69743e8
sunary <v2nhat@gmail.com>
2016-05-10 20:32:27 +0700

    remove build.info file

--- setup.py
+++ setup.py
@@ -3,15 +3,10 @@ __author__ = 'sunary'
 
 import os
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
 
 def __path(filename):
     return os.path.join(os.path.dirname(__file__), filename)
-
-build = 0
-if os.path.exists(__path('build.info')):
-    build = open(__path('build.info')).read().strip()
 
 with open('README.md') as fo:
     readme = fo.read()
@@ -22,6 +17,7 @@ with open('LICENSE') as fo:
 with open('CHANGES.md') as fo:
     changes = fo.read()
 
+build = 5
 version = '0.1.{0}'.format(build)
 
 setup(
